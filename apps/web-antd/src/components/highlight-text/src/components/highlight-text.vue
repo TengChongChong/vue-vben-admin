289489197b48b6194f7eb85db4753bf22c4386f9
TengChongChong <26172583@qq.com>
2024-09-18 21:24:22 +0800

    feat: add highlight-text

---
+++ apps/web-antd/src/components/highlight-text/src/components/highlight-text.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+  // 高亮文字
+  keyword: string;
+  // 文字
+  text: string;
+}>();
+</script>
+
+<template>
+  <template
+    v-for="(fragment, i) in text
+      .toString()
+      .split(new RegExp(`(?<=${keyword})|(?=${keyword})`, 'i'))"
+  >
+    <span
+      v-if="fragment.toLowerCase() === keyword.toLowerCase()"
+      :key="i"
+      class="highlight-text"
+    >
+      {{ fragment }}
+    </span>
+    <template v-else>{{ fragment }}</template>
+  </template>
+</template>
+
+<style scoped>
+.highlight-text {
+  color: hsl(var(--primary));
+}
+</style>
