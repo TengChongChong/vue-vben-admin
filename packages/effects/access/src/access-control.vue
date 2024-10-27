8e010ddf70570e3e3b4f08490ff55505228e99b4
TengChongChong <26172583@qq.com>
2024-10-27 14:40:55 +0800

    ci: 默认type改为code

--- packages/effects/access/src/access-control.vue
+++ packages/effects/access/src/access-control.vue
@@ -30,7 +30,7 @@ defineOptions({
 
 const props = withDefaults(defineProps<Props>(), {
   codes: () => [],
-  type: 'role',
+  type: 'code',
 });
 
 const { hasAccessByCodes, hasAccessByRoles } = useAccess();
