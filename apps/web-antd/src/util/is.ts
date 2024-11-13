242654e5464f8068097f318944585c0ac2c90afe
TengChongChong <26172583@qq.com>
2024-11-13 20:44:28 +0800

    feat: add is

---
+++ apps/web-antd/src/util/is.ts
@@ -0,0 +1,74 @@
+export {
+  isArguments,
+  isArrayBuffer,
+  isArrayLike,
+  isArrayLikeObject,
+  isBoolean,
+  isBuffer,
+  isDate,
+  isElement,
+  isEmpty,
+  isEqual,
+  isEqualWith,
+  isError,
+  isFinite,
+  isFunction,
+  isLength,
+  isMap,
+  isMatch,
+  isMatchWith,
+  isNative,
+  isNil,
+  isNull,
+  isNumber,
+  isObjectLike,
+  isPlainObject,
+  isRegExp,
+  isSafeInteger,
+  isSet,
+  isString,
+  isSymbol,
+  isTypedArray,
+  isUndefined,
+  isWeakMap,
+  isWeakSet,
+} from 'lodash-es';
+const toString = Object.prototype.toString;
+
+export function is(val: unknown, type: string) {
+  return toString.call(val) === `[object ${type}]`;
+}
+
+export function isDef<T = unknown>(val?: T): val is T {
+  return val !== undefined;
+}
+
+export function isNullOrUnDef(val: unknown): val is null | undefined {
+  return !isDef(val) || val === null;
+}
+
+export function isBlank(str: null | string | undefined) {
+  return isNullOrUnDef(str) || str === '';
+}
+
+export function isNotBlank(str: null | string | undefined) {
+  return !isBlank(str);
+}
+
+// TODO 此处 isObject 存在歧义
+export function isObject(val: any): val is Record<any, any> {
+  return val !== null && is(val, 'Object');
+}
+
+// TODO 此处 isArray 存在歧义
+export function isArray(val: any): val is Array<any> {
+  return val && Array.isArray(val);
+}
+
+export function isWindow(val: any): val is Window {
+  return typeof window !== 'undefined' && is(val, 'Window');
+}
+
+export const isServer = typeof window === 'undefined';
+
+export const isClient = !isServer;
