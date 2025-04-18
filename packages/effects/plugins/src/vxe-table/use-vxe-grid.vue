4255e051e229a391aebd92af67fb647fdd9b590e
TengChongChong <26172583@qq.com>
2025-04-18 17:10:05 +0800

    ci: 表格标题加粗

--- packages/effects/plugins/src/vxe-table/use-vxe-grid.vue
+++ packages/effects/plugins/src/vxe-table/use-vxe-grid.vue
@@ -365,7 +365,7 @@ onUnmounted(() => {
       <!-- 左侧操作区域或者title -->
       <template v-if="showToolbar" #toolbar-actions="slotProps">
         <slot v-if="showTableTitle" name="table-title">
-          <div class="mr-1 pl-1 text-[1rem]">
+          <div class="mr-1 pl-1 text-[1rem] font-medium">
             {{ tableTitle }}
             <VbenHelpTooltip v-if="tableTitleHelp" trigger-class="pb-1">
               {{ tableTitleHelp }}
